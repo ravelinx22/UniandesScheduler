30cb46e712d562602d18f62f355308041759f62f
William Ravelo <drummerwilliam@gmail.com>
2018-07-17 19:51:14 -0500

    remove old files and made dev database setup

--- client/app/components/App/App.js
+++ client/app/components/App/App.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react';
 
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-
 const App = ({ children }) => (
-  <>
-    <Header />
-
-    <main>
-      {children}
-    </main>
-
-    <Footer />
-  </>
+	<div>
+		{children}
+	</div>
 );
 
 export default App;
--- client/app/index.js
+++ client/app/index.js
@@ -10,11 +10,7 @@ import {
 
 import App from './components/App/App';
 import NotFound from './components/App/NotFound';
-
 import Home from './components/Home/Home';
-
-import HelloWorld from './components/HelloWorld/HelloWorld';
-
 import './styles/styles.scss';
 
 render((
@@ -22,7 +18,6 @@ render((
     <App>
       <Switch>
         <Route exact path="/" component={Home}/>
-        <Route path="/helloworld" component={HelloWorld}/>
         <Route component={NotFound}/>
       </Switch>
     </App>

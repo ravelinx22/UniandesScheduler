7ccc98e865c512ffe2813b02e9bda8ba4b25259b
William Ravelo <drummerwilliam@gmail.com>
2018-07-17 22:29:15 -0500

    left side

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -8,30 +8,51 @@ localizer(moment)
 class Home extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			records: []
+		}
 	}
 
 	componentDidMount() {
+		this.searchDepartment("ISIS");
+	}
+
+	searchDepartment(depto) {
+		fetch("https://registroapps.uniandes.edu.co/oferta_cursos/api/get_courses.php?term=201820&ptrm=1&prefix=" + depto + "&campus=&attr=&attrs=",{
+			method: 'GET',
+		}).then((ans) => {
+			return ans.json();
+		}).then((json) => {
+			console.log(json);
+			this.setState({
+				records: json.records
+			});
+		});
 	}
 
 	render() {
 		return (
-			<div className="app">
-				<div className="calendarApp">
-					<BigCalendar
-						selectable
-						events={events}
-						defaultView={BigCalendar.Views.MONTH}
-						scrollToTime={new Date(1970, 1, 1, 6)}
-						defaultDate={new Date(2015, 3, 12)}
-						onSelectEvent={event => alert(event.title)}
-						onSelectSlot={slotInfo =>
-								alert(
-									`selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
-									`\nend: ${slotInfo.end.toLocaleString()}` +
-									`\naction: ${slotInfo.action}`
-								)
-						}
-					/>	
+			<div>
+				<div className="col-md-2 side">
+				</div>
+				<div className="col-md-10 app">
+					<div className="calendarApp">
+						<BigCalendar
+							selectable
+							events={events}
+							defaultView={BigCalendar.Views.MONTH}
+							scrollToTime={new Date(1970, 1, 1, 6)}
+							defaultDate={new Date(2015, 3, 12)}
+							onSelectEvent={event => alert(event.title)}
+							onSelectSlot={slotInfo =>
+									alert(
+										`selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
+										`\nend: ${slotInfo.end.toLocaleString()}` +
+										`\naction: ${slotInfo.action}`
+									)
+							}
+						/>	
+					</div>
 				</div>
 			</div>
 		);
